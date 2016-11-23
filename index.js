46943ece6f10e8400f888d6da77d415f28c64408
Espen Klem <espen.klem@gmail.com>
2016-11-23 05:56:59 -0500

    Getting data out! Next: write to JSONstream and index

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 // Modules and stuff required
 var gsheets = require('gsheets')
 var fs = require('fs-extra')
-var gheetsKey = '15htqMLMdbxi4csfmOyR1r6aMfZO2R2bOBacS834kfvU'
-var gheetsWorkseeht = '15htqMLMdbxi4csfmOyR1r6aMfZO2R2bOBacS834kfvU'
+var gsheetsKey = '15htqMLMdbxi4csfmOyR1r6aMfZO2R2bOBacS834kfvU'
+var gsheetsWorksheet = 'redwine'
 var columns = ['Varenummer','Varenavn','Volum','Pris','Literpris','Varetype','Farge','Lukt','Smak','Land','Distsrikt','Underdistrikt','Argang','Rastoff','Alkohol']
-var filter = 'Rødvin' // '' to not filter
 
 // Get csv-file as 'data' (object)
-gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, result) {
+gsheets.getWorksheet(gsheetsKey, gsheetsWorksheet, function(err, result) {
   if (err) {
     console.dir(err)
   }
-  // Do stuff to object.
-  console.dir(JSON.stringify(result.data))
   // Iterating through rows of data from spreadsheet
-  //for (var i=0; i<result.data.length; i++) {
-  //  var obj = result.data[i]
-  //}
-  
+  for (var i=0; i<result.data.length; i++) {
+    //var obj = result.data[i]
+    console.log(result.data[i])
+  }
   // Store object in JSON file
 
-}
+})
