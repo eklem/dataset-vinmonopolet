de238978ebcb61e84cd037d865568f3a24a94ae9
Espen Klem <espen.klem@gmail.com>
2016-11-29 04:43:16 -0500

    First dataset ready. A little hackish solution to write the data as JSONstream-type to file

--- google-to-JSONstream-file.js
+++ google-to-JSONstream-file.js
@@ -2,8 +2,9 @@
 const gsheets = require('gsheets')
 const fs = require('fs-extra')
 const JSONStream = require('JSONStream')
+const file = 'dataset-vinmonopolet.json'
 const gsheetsKey = '15htqMLMdbxi4csfmOyR1r6aMfZO2R2bOBacS834kfvU'
-const gsheetsWorksheet = 'redwine' // 'products' (all), 'redwine', 'whitewine', 'sparkling' and 'test' available so far
+const gsheetsWorksheet = 'sparkling' // 'products' (all), 'redwine', 'whitewine', 'sparkling' and 'test' available so far
 const columns = ['Varenummer','Varenavn','Volum','Pris','Literpris','Varetype','Farge','Lukt','Smak','Land','Distsrikt','Underdistrikt','Argang','Rastoff','Alkohol']
 
 // Get csv-file as 'data' (object)
@@ -11,15 +12,28 @@ gsheets.getWorksheet(gsheetsKey, gsheetsWorksheet, function(err, result) {
   if (err) {
     console.dir(err)
   }
+  var output =  ''
   // Iterating through rows of data from spreadsheet
   for (var i=0; i<result.data.length; i++) {
-    console.log(' ')
+    // Object to fill with key value pairs
+    wine = {}
+    //console.log('Wine object empty? ' + JSON.stringify(wine))
     for (var key in result.data[i]){
       if (typeof result.data[i][key] !== 'function' && columns.indexOf(key) > -1) {
         // key in object exists, and matches something in columns array
-        console.log(key + ' ' + result.data[i][key]);
-        // add to obj and write/add obj to JSON stream file
+        // Add key/value to wine object
+        wine[key] = result.data[i][key]
       }
     }
+    //console.dir(JSON.stringify(wine))
+    output += JSON.stringify(wine)
+    output += '\n'
+    // Push object to arrray that will be written to file (or write it directly?)
   }
+  //console.log(output)
+  fs.outputFile(file, output, 'utf8', function (err) {
+   if (err) {
+     console.log('Error: \n' + err)
+   }
+  })
 })
