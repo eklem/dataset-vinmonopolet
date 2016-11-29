f977e90c129ad556560f4cf28526851e53f35002
Espen Klem <espen.klem@gmail.com>
2016-11-29 06:47:37 -0500

    Cleanup code and package.json

--- google-to-JSONstream-file.js
+++ google-to-JSONstream-file.js
@@ -1,10 +1,9 @@
 // Modules and stuff required
 const gsheets = require('gsheets')
 const fs = require('fs-extra')
-const JSONStream = require('JSONStream')
 const file = 'dataset-vinmonopolet.str'
 const gsheetsKey = '15htqMLMdbxi4csfmOyR1r6aMfZO2R2bOBacS834kfvU'// Gsheet's key to csv-products file converted to Gsheet
-const gsheetsWorksheet = 'test' // 'products' (all), 'redwine', 'whitewine', 'sparkling' and 'test' available so far
+const gsheetsWorksheet = 'products' // 'products' (all), 'redwine', 'whitewine', 'sparkling' and 'test' available so far
 const columns = ['Varenummer','Varenavn','Volum','Pris','Literpris','Varetype','Farge','Lukt','Smak','Land','Distsrikt','Underdistrikt','Argang','Rastoff','Alkohol']
 
 // Get csv-file as 'data' (object)
@@ -17,10 +16,9 @@ gsheets.getWorksheet(gsheetsKey, gsheetsWorksheet, function(err, result) {
   for (var i=0; i<result.data.length; i++) {
     // Object to fill with key value pairs
     wine = {}
-    //console.log('Wine object empty? ' + JSON.stringify(wine))
     for (var key in result.data[i]){
       if (typeof result.data[i][key] !== 'function' && columns.indexOf(key) > -1) {
-        // key in object exists, and matches something in columns array
+        // key in wine object exists, && matches something in columns array
         // Add key/value to wine object
         wine[key] = result.data[i][key]
       }
@@ -28,9 +26,8 @@ gsheets.getWorksheet(gsheetsKey, gsheetsWorksheet, function(err, result) {
     //console.dir(JSON.stringify(wine))
     output += JSON.stringify(wine)
     output += '\n'
-    // Push object to arrray that will be written to file (or write it directly?)
   }
-  //console.log(output)
+  // Writing output to file
   fs.outputFile(file, output, 'utf8', function (err) {
    if (err) {
      console.log('Error: \n' + err)
